9510275d799008ec1eaa4367d5970dfebd3b021a
Dmytro Mai <dm933@cam.ac.uk>
2024-08-25 12:01:59 +0100

    added profile pic

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Personal Portfolio website using Next.JS, tailwind, typescript and sentry.
 - shadcn/ui https://ui.shadcn.com/
 ## TODO
 - [x] Add personal projects and experiences
-- [ ] Add a profile picture
+- [x] Add a profile picture
 - [ ] Add a slider which displays my skills
 - [ ] Change project pictures
 - [ ] Add dialog card when pressing on projects to display details
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,13 +12,19 @@ const Hero = () => {
                 <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
                 <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue' />
             </div>
-            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-                {/* Radial gradient for the container to give a faded look */}
+            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] flex items-start justify-center absolute top-0 left-0">
+                <div className="relative w-48 h-48 rounded-full overflow-hidden z-0">
+                        <img 
+                            src="/profile_pic3.jpeg" 
+                            alt="Profile Image" 
+                            className="w-full h-full object-cover object-center rounded-full"
+                        />
+                    </div>
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
                 
             </div>
             <div className='flex justify-center relative my-20 z-10'>
-                <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center relative'>
                     <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
                         Made with Next.js
                     </h2>
